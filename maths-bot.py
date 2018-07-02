d9279356582c73385104a3c5a4acb9b2d7d1351c
Aaron Cuthbertson <3aaron3@gmail.com>
2018-07-02 10:14:23 +0100

    Added support for the full archive of problems.

--- maths-bot.py
+++ maths-bot.py
@@ -4,16 +4,20 @@ import discord
 import requests
 from bs4 import BeautifulSoup
 from random import randint
+import re
+import math
 
 WEEKLY_URL = "https://www.kcl.ac.uk/mathsschool/weekly-maths-challenge/weekly-maths-challenge.aspx" #All problems are sourced from the Kings Maths School Seven Day Maths website
+#Problems 101 onwards have their own pages, linked from the weekly URL
+ARCHIVE_URLS = ["https://www.kcl.ac.uk/mathsschool/weekly-maths-challenge/previouschallenges.aspx", "https://www.kcl.ac.uk/mathsschool/weekly-maths-challenge/recentchallenges.aspx", "https://www.kcl.ac.uk/mathsschool/weekly-maths-challenge/challenges-41-60.aspx", "https://www.kcl.ac.uk/mathsschool/weekly-maths-challenge/challenges-61-80.aspx", "https://www.kcl.ac.uk/mathsschool/weekly-maths-challenge/challenges-81-100.aspx"]
 WEEKLY_TEXT = "This week's challenge!\n--------------------------\n" #Beginning of the message that gets posted when the weekly problem updates
-TOKEN = "NDYxODAwNTQ3MTI1NTU5MzA2.DhYlOg.87JFcgMjJf4IvHOEeHijA9JWeus"
+TOKEN = "XXX"
 NOTIF_CHANNEL_ID = 'XXX'
 TARGET_CHANNEL_ID = 'XXX'
 
 #Function to generate an array of all the (absolute) links to each problem.
-#Currently does only problems 101 onwards, as the rest are stored differently.
-#Due to how requests_html seems to work, the links seem to be placed in the list in a random order each time.
+#Used only for problems 101 onwards, as the rest are stored differently. Urls are in ARCHIVE_URLS
+#Due to how requests_html seems to work, the links seem to be placed in the list in a random order each time
 def getLinks():
         import requests_html
         session = requests_html.HTMLSession()
@@ -31,31 +35,74 @@ def getLinks():
 
         return links #Returns the list
 
+#Used to determine when the problem description has ended. Includes every possible text signalling the description has finished, as of 02/07/2018
+def endOfProblem(element):
+    result = True
+    if ("view the solution" in str(element).lower()):
+        result = False
+
+    if ("view the solutions" in str(element).lower()):
+        result = False
+
+    if ("view solution" in str(element).lower()):
+        result = False
+
+    if ("think you can solve it" in str(element).lower()):
+        result = False
+
+    if ("think you have the answer" in str(element).lower()):
+        result = False
+
+    if ("view the solution attempts" in str(element).lower()):
+        result = False
+
+    if ("an alternative solution was submitted" in str(element).lower()):
+        result = False
+
+    return result
+
 #Function to get the Title of the problem from a given URL. Returns a string
-def title(url):
+#archiveNum is optional, used if the problem is before #101
+def title(url, archiveNum=0):
     r = requests.get(url)
     html = r.text
     soup = BeautifulSoup(html, 'html.parser')
-    title = soup.h3.next_sibling.next_sibling
+    if url not in ARCHIVE_URLS: #If the problem has its own webpage; ie. problems 101 onwards
+        title = soup.h3.next_sibling.next_sibling
+    else:
+        anchors = soup.find_all(class_="sys_trigger", string=re.compile("Weekly")) #Gets all title tags on the page
+        archiveNum  = abs(archiveNum-20) + 19 #Because they are found in reverse order, archiveNum must be adjusted
+        title = anchors[archiveNum % 20] #Modulo is used because challenges are split into groups of 20
     return title.getText()
 
 #Function to get the Description of the problem from a given URL. Returns a string
-def description(url):
+def description(url, archiveNum=-1):
     r = requests.get(url)
     html = r.text
     soup = BeautifulSoup(html, 'html.parser')
-
     text = ""
-    element = soup.h3.next_sibling.next_sibling #Description always starts after the Title, which occurs 2 'siblings' after the first h3 tag
-    #The description is a variable length, but always ends with an <hr/> tag.
-    #This loops until that tag is reached, adding the text (that isn't Null, or blank as there seems to be a lot of whitespace) to the Decription
-    while "<hr/>" not in str(element):
-        element = element.next_sibling
-        if (str(element).rstrip()) != "" and element != None:
-            text += element.getText()
-        else:
-            text += "\n"
-
+    if url not in ARCHIVE_URLS: #If the problem has its own webpage; ie. problems 101 onwards
+        element = soup.h3.next_sibling.next_sibling #Description always starts after the Title, which occurs 2 'siblings' after the first h3 tag
+        #The description is a variable length, but always ends with an <hr/> tag.
+        #This loops until that tag is reached, adding the text (that isn't Null, or blank as there seems to be a lot of whitespace) to the Decription
+        while "<hr/>" not in str(element):
+            element = element.next_sibling
+            if (str(element).rstrip()) != "" and element != None:
+                text += element.getText()
+            else:
+                text += "\n"
+    else:
+        anchors = soup.find_all(class_="sys_trigger", string=re.compile("Weekly")) #Gets all title tags
+        archiveNum  = abs(archiveNum-20) + 19 #Because they are found in reverse order, archiveNum must be adjusted
+        elements = anchors[archiveNum % 20].next_sibling.contents #Description is nested below the next tag ('sibling') from the title. Modulo is used because challenges are split into groups of 20.
+        j = 0
+         #This loops until the problem description has ended, adding the text (that isn't Null, or blank as there seems to be a lot of whitespace) to the Decription
+        while endOfProblem(elements[j]) and j != len(elements)-1:
+            if (str(elements[j]).rstrip()) != "" and elements[j] != None:
+                text += elements[j].getText()
+            else:
+                text += "\n"
+            j += 1
     return text
 
 #Function to return the message the bot should send, containing the problem's Title and Description.
@@ -70,13 +117,18 @@ def question(random):
         titleVar = title(WEEKLY_URL)
         descriptionVar = description(WEEKLY_URL)
     else:
-        rand = randint(0, len(links)-1) #Generates a random number...
-        titleVar = title(links[rand]) #...and gets the Title...
-        descriptionVar = description(links[rand]) #and Description of the challenge from the link found at that index.
+        #1 less than len(links) for all problems with their own page, +100 for all other archived problems.
+        rand = randint(0, len(links)-1+100) #Generates a random number...
+        if rand >= 100: #(ie. problem has its own webpage)
+            titleVar = title(links[rand-100]) #...and gets the Title...
+            descriptionVar = description(links[rand-100]) #and Description of the challenge from the link found at that index.
 
+        else:
+            #...gets the title and description for that problem. math.floor(rand / 20) is to get the correct group and URL
+            titleVar = title(ARCHIVE_URLS[math.floor(rand / 20)], rand) 
+            descriptionVar = description(ARCHIVE_URLS[math.floor(rand / 20)], rand)
     message = "**"+titleVar+"**\n"+descriptionVar #Returns the message for the bot to send; the Title in bold and the challenge description after it
     return message
-
 
 client = discord.Client()
 
